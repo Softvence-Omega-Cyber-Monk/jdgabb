27ce1cfd38299cfd33fc10f037ea7f89c8f11a59
Mahedi Hasan Jihad <mahdijihad001@gmail.com>
2026-01-20 09:37:13 +0600

    Update Api

--- src/app/module/UpdateProject/UpdateProjectRouter.ts
+++ src/app/module/UpdateProject/UpdateProjectRouter.ts
@@ -9,7 +9,7 @@ UpdateProjectRouter.get("/getProject/:projectId", getProjectController);
 UpdateProjectRouter.post("/:projectId/tasks", createTaskController);
 UpdateProjectRouter.delete("/:taskId/deletetask", deleteTaskController);
 UpdateProjectRouter.patch("/update/tasks/:taskId", updateTaskController);
-UpdateProjectRouter.get("/tasks/:taskId/parents", getAllParentTasks);
+UpdateProjectRouter.get("/tasks/:taskId/parents/:projectId", getAllParentTasks);
 UpdateProjectRouter.get("/tasks/child/:taskId", getTaskWithChildren);
 UpdateProjectRouter.get("/all/project/:userId", getSingleUserAllProject);
 export default UpdateProjectRouter;
